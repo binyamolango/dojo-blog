624d37683806029a4d441261e14dc5d75b662892
binyamyohannes <olangobinyam@gmail.com>
2023-12-19 18:10:05 +0300

    Update props

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,12 +1,13 @@
 const BlogList = (props) => {
-  const blogs = props.blogs;
+  const { blogs, title } = props;
 
   return (
     <div className="blog-list">
+      <h1>{ title }</h1>
       {
         blogs.map(blog => (
           <>
-            <div className="blog-preview" key={blog.id}>
+            <div className="blog-preview" key={ blog.id }>
               <h2>{ blog.title }</h2>
               <p>Written by: { blog.author }</p>
             </div>
