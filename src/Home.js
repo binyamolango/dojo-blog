35cbc843213461100dc1199f1dc55182265e2754
binyamyohannes <olangobinyam@gmail.com>
2023-12-19 15:34:09 +0300

    Update home

--- src/Home.js
+++ src/Home.js
@@ -2,14 +2,17 @@ import { useState } from "react";
 
 const Home = () => {
   const [ name, setName ] = useState('Binyam');
+  const [ age, setAge ] = useState(25);
   const handleClick = () => {
     setName('Micky');
+    setAge(28);
   }
 
   return (
     <div className="home">
       <h1>Homepage</h1>
       <p>{name}</p>
+      <p>{age}</p>
       <button onClick={handleClick}>Click me</button>
     </div>
   );
