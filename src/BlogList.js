37b99a3add8ce20c55add70c87dc33f5b3c7d991
binyamyohannes <olangobinyam@gmail.com>
2023-12-20 14:00:54 +0300

    Using multiple components

--- src/BlogList.js
+++ src/BlogList.js
@@ -10,6 +10,7 @@ const BlogList = (props) => {
             <div className="blog-preview" key={ blog.id }>
               <h2>{ blog.title }</h2>
               <p>Written by: { blog.author }</p>
+              <button>Delete</button>
             </div>
           </>
         ))
