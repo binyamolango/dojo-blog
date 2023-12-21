c01240dffa59e7abb718177e62f257528f36a844
binyamyohannes <olangobinyam@gmail.com>
2023-12-21 12:42:24 +0300

    Update blog details

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,11 +1,21 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
   const { id } = useParams();
+  const { data: blogs, isPending, error } = useFetch('http://localhost:8080/blogs/' + id);
 
   return (
     <div className="blog-details">
-      <h1>Blog Details - {id}</h1>
+      { isPending && <div> Loading... </div> }
+      { error && <div>{ error }</div> }
+      { blogs &&  (
+        <article>
+          <h2>{blogs.title}</h2>
+          <p>Written by: { blogs.author }</p>
+          <div>{blogs.body}</div>
+        </article>
+      )}
     </div>
   );
 }
