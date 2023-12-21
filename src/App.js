e53ed2bc4d187883d5c2e9ee27f67ab2f69bc375
binyamyohannes <olangobinyam@gmail.com>
2023-12-21 11:26:42 +0300

    Add router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import Home from './Home';
 import Navbar from './Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Create from './Create';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/create' element={<Create />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/BlogList.js
+++ src/BlogList.js
@@ -6,12 +6,10 @@ const BlogList = (props) => {
       <h1>{ title }</h1>
       {
         blogs.map(blog => (
-          <>
-            <div className="blog-preview" key={ blog.id }>
-              <h2>{ blog.title }</h2>
-              <p>Written by: { blog.author }</p>
-            </div>
-          </>
+          <div className="blog-preview" key={ blog.id }>
+            <h2>{ blog.title }</h2>
+            <p>Written by: { blog.author }</p>
+          </div>
         ))
       }
     </div>
