ff099a29e68ebe40a59ecaede90d435f39690121
binyamyohannes <olangobinyam@gmail.com>
2023-12-24 12:22:04 +0300

    Add form to create

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,30 @@
 const Create = () => {
   return (
     <div className="create">
-      <h1>New Blog</h1>
+      <form>
+        <label htmlFor="title">Blog title:</label>
+        <input
+          type="text"
+          id="title"
+        />
+
+        <label htmlFor="body">Blog body:</label>
+        <textarea
+          name="body"
+          id="body"
+        >
+        </textarea>
+
+        <select
+          name="author"
+          id="author"
+        >
+          <option value="mario">Mario</option>
+          <option value="yoshi">Yoshi</option>
+        </select>
+
+        <button type="submit">Add Blog</button>
+      </form>
     </div>
   );
 }
